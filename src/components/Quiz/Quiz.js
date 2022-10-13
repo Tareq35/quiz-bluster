8e1b4695dc34e45f25abe94d4899d875176f25cf
Tareq35 <a.t.tariqulislam@gmail.com>
2022-10-13 16:54:22 +0600

    quiz options dynamically added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,22 +5,29 @@ import QuizItem from '../QuizItem/QuizItem';
 const Quiz = () => {
     const { courseId } = useParams();
     const [quiz, setQuiz] = useState({});
+
     useEffect(() => {
         fetch(`https://openapi.programming-hero.com/api/quiz/${courseId}`)
-        .then(res => res.json())
-        .then(data => setQuiz(data))
+            .then(res => res.json())
+            .then(data => setQuiz(data.data))
     }, [courseId])
 
-
+    if(!quiz.id) return <div className='flex justify-center'>loading....</div>
 
     console.log(quiz);
     return (
         <div>
-            <h1 className='text-center'>Question of {}</h1>
-            <QuizItem
-            
-            ></QuizItem>
-            
+            <h1 className='text-center'>Question of {quiz?.name}</h1>
+            <div>
+                {
+                    quiz.questions.map(question => <QuizItem
+                        key={question.id}
+                        question={question}
+                    ></QuizItem>)
+                }
+            </div>
+
+
         </div>
     );
 };
--- src/components/QuizItem/QuizItem.js
+++ src/components/QuizItem/QuizItem.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import Quiz from '../Quiz/Quiz';
 
-const QuizItem = () => {
+const QuizItem = ({ question }) => {
+
     const [check, setCheck] = useState(false)
+
+    // const { question, options } = question;
     return (
         <div>
             <div className='flex justify-evenly mb-8'>
-                <h2>Razib is a abal</h2>
+                <h2>{question.question}</h2>
 
                 <p>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -16,30 +20,16 @@ const QuizItem = () => {
             </div>
 
             <div className='flex flex-col gap-2'>
-                <div className='w-6/12 mx-auto flex border rounded-md border-slate-400 py-10 px-5'>
-                    <input className='' type="radio" name="" id="" />
-                    <p>
-                        option-1
-                    </p>
-                </div>
-                <div className='w-6/12 mx-auto flex border rounded-md border-slate-400 py-10 px-5'>
-                    <input className='' onChange={(e) => setCheck(e.target.checked)} checked={check} type="radio" name="" id="" />
-                    <p>
-                        option-2
-                    </p>
-                </div>
-                <div className='w-6/12 mx-auto flex border rounded-md border-slate-400 py-10 px-5'>
-                    <input className='' type="radio" name="" id="" />
-                    <p>
-                        option-3
-                    </p>
-                </div>
-                <div className='w-6/12 mx-auto flex border rounded-md border-slate-400 py-10 px-5'>
-                    <input className='' type="radio" name="" id="" />
-                    <p>
-                        option-4
-                    </p>
-                </div>
+                {
+                    question.options.map((option, index) => (
+                        <div key={index} onClick={() => setCheck(!check)} className='cursor-pointer w-6/12 mx-auto flex border rounded-md border-slate-400 py-10 px-5'>
+                            <input className='' onChange={() => { }} checked={check} type="radio" name="" id="" />
+                            <p>
+                                {option}
+                            </p>
+                        </div>
+                    ))
+                }
             </div>
 
         </div>
